03f84e1ac9975cdd6da5925922de8180d72757ac
sosisol <sosnasolomon14@gmail.com>
2022-12-09 13:36:27 -0800

    mandatory tasks from 2 to 9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+/**
+  * main - entry block
+  * description - prints the lowercase alphabet in reverse
+  * Return: 0
+**/
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}#include <stdio.h>
+/**
+ *   * main - entry block
+ *     * description - prints the lowercase alphabet in reverse
+ *       * Return: 0
+ *       **/
+int main(void)
+{
+		char c;
+
+			for (c = 'z' ; c >= 'a'; c--)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}#include <stdio.h>
+/**
+ *   * main - entry block
+ *     * description - prints the lowercase alphabet in reverse
+ *       * Return: 0
+ *       **/
+int main(void)
+{
+		char c;
+
+			for (c = 'z' ; c >= 'a'; c--)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}#include <stdio.h>
+/**
+ *   * main - entry block
+ *     * description - prints the lowercase alphabet in reverse
+ *       * Return: 0
+ *       **/
+int main(void)
+{
+		char c;
+
+			for (c = 'z' ; c >= 'a'; c--)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}#include <stdio.h>
+/**
+ *   * main - entry block
+ *     * description - prints the lowercase alphabet in reverse
+ *       * Return: 0
+ *       **/
+int main(void)
+{
+		char c;
+
+			for (c = 'z' ; c >= 'a'; c--)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}#include <stdio.h>
+/**
+ *   * main - entry block
+ *     * description - prints the lowercase alphabet in reverse
+ *       * Return: 0
+ *       **/
+int main(void)
+{
+		char c;
+
+			for (c = 'z' ; c >= 'a'; c--)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}#include <stdio.h>
+/**
+ *   * main - entry block
+ *     * description - prints the lowercase alphabet in reverse
+ *       * Return: 0
+ *       **/
+int main(void)
+{
+		char c;
+
+			for (c = 'z' ; c >= 'a'; c--)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}
