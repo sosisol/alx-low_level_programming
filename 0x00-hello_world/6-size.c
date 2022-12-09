1876e8d4bc4ac04dbd2748174fe4d25f1614a0d8
sosisol <sosnasolomon14@gmail.com>
2022-12-09 11:11:20 -0800

    dfadf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main -Entry point
  *
@@ -7,10 +8,16 @@
  */
 int main(void)
 {
-printf("size of a char: %zu byte(s)\n", sizeof(char));
-printf("size of an int: %zu byte(s)\n", sizeof(int));
-printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("size of a float: %zu byte(s)\n", sizeof(float));
-return (0);
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+			printf("Size of an int: %zu byte(s)\n", sizeof(int));
+
+				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+
+					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+
+						printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+							return (0);
+
 }
