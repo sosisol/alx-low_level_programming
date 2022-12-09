ee1bd383dc3791b27540675c0012ecec5d4f1982
fekadum <40325091+fekadum@users.noreply.github.com>
2022-12-09 12:04:40 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,13 @@
 
 /**
  *
- *  *main - Entry point
+ *main - Entry point
  *
- *   *Return: Always 0
+ *Return: Always 0
  *
- *    */
-
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle\n");
-
 			return (0);
-
 }
