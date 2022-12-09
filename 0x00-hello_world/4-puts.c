8f137867947773bd7506d14b85bc6850039e88e0
sosisol <sosnasolomon14@gmail.com>
2022-12-09 10:31:08 -0800

    last commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-
-/**
-*main - Entry point
-*Description: 'the program's description'
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+#include <stdio.h>                                                                                                                                       
+                                                                                                                                                         
+/**                                                                                                                                                      
+*main - Entry point                                                                                                                                      
+*Description: 'the program's description'                                                                                                                
+ *                                                                                                                                                       
+ * Return: Always 0 (Success)                                                                                                                            
+ */                                                                                                                                                      
+int main(void)                                                                                                                                           
+{                                                                                                                                                        
+                puts("\"Programming is like building a multilingual puzzle");                                                                            
+                        return (0);                                                                                                                      
 }
