e91633faefe0b081a6fdcb83b0a28f0d1ea7c2d6
sosisol <sosnasolomon14@gmail.com>
2022-12-08 22:38:33 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-
+#include <stdio.h>                                                              
 /**
- * main - entry point 
- *
- * Return: always 0 (success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+ * main - entry point                       
+ * Return: always 0 (success)                                                                                               
+ */                                                                               
 
+int main(void)                                                                                                              
+
+{                                                                                                                          
+	        puts("\"Programming is like building a multilingual puzzle");                                                       
+		return (0);          
 }
+
