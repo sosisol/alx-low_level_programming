abedbd75ac41f7577ec4d6b16dd352cefc961723
sosisol <sosnasolomon14@gmail.com>
2022-12-14 03:41:46 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Test function for positive or negative
- * Return: 0
+ *  Return: 0
  */
 
 int main(void)
@@ -11,5 +11,6 @@ int i;
 
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
