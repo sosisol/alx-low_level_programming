76eee6bc1d5703f946f93f78e2e4ca9e601f68f2
sosisol <sosnasolomon14@gmail.com>
2022-12-16 01:24:28 -0800

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
  * @n: integer to be printed
  */
 void print_number(int n)
 {
+
 unsigned int n1;
 
 if (n < 0)
@@ -15,12 +15,13 @@ _putchar('-');
 }
 else
 {
-n1 = n
+n1 = n;
 }
-
 if (n1 / 10)
 {
 print_number(n1 / 10);
 }
 _putchar((n1 % 10) + '0');
 }
+
+
