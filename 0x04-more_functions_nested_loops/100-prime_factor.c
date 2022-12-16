0b772972c7b017c0968e26a86b3fbe7c3509b3b5
sosisol <sosnasolomon14@gmail.com>
2022-12-16 01:04:40 -0800

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,13 +14,13 @@ long int i;
 
 n = 612852475143;
 max = -1;
-}
 
 while (n % 2 == 0)
 {
 max = 2;
 n /= 2;
 }
+
 for (i = 3; i <= sqrt(n); i = i + 2)
 {
 while (n % i == 0)
@@ -29,11 +29,9 @@ max = i;
 n = n / i;
 }
 }
-
 if (n > 2)
 max = n;
 
 printf("%ld\n", max);
-
 return (0);
 }
