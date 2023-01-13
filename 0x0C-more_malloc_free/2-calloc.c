efdfe0b972fcfe8c3cb2508b0db713d457088b3c
sosisol <sosnasolomon14@gmail.com>
2023-01-13 13:36:42 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,3 +18,22 @@ s[i] = b;
 }
 return (s);
 }
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+return (NULL);
+_memset(ptr, 0, nmemb * size);
+return (ptr);
+
+}
