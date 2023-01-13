59f33b7f7bcaf189fda76f8e8621a7cf2955f6c3
sosisol <sosnasolomon14@gmail.com>
2023-01-13 13:28:39 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,7 +7,7 @@
  * @b: char to copy
  * @n: number of times to copy b
  *
- * Return: pointer to the memory area s
+ *  Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
